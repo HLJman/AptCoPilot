276d9361e4651c17dad58625eb9270883e6ba7f5
joshadambell <joshadambell@me.com>
2018-01-02 15:30:23 -0700

    update ui to be single handler

--- route/route.go
+++ route/route.go
@@ -20,14 +20,9 @@ func Register(root *goji.Mux) {
 	subMux.HandleFunc(pat.Get("/properties/:id"), handler.Property)
 
 	// UI
-	root.Handle(pat.New("/src"), http.FileServer(http.Dir("./assets/src")))
-	root.HandleFunc(pat.New("/properties"), func(w http.ResponseWriter, r *http.Request) {
+	root.HandleFunc(pat.New("/*"), func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./assets/index.html")
 	})
-	root.HandleFunc(pat.New("/contact"), func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./assets/index.html")
-	})
-	root.Handle(pat.New("/*"), http.FileServer(http.Dir("./assets")))
 }
 
 func corsHandler(h http.Handler) http.Handler {
