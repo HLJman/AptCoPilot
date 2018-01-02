290955e4608bf668622ffe33e0b45b7099d1bca7
joshadambell <joshadambell@me.com>
2018-01-02 15:58:29 -0700

    revert routing changes

--- route/route.go
+++ route/route.go
@@ -20,13 +20,6 @@ func Register(root *goji.Mux) {
 	subMux.HandleFunc(pat.Get("/properties/:id"), handler.Property)
 
 	// UI
-	root.Handle(pat.New("/src"), http.FileServer(http.Dir("./assets/src")))
-	root.HandleFunc(pat.New("/properties"), func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./assets/index.html")
-	})
-	root.HandleFunc(pat.New("/contact"), func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./assets/index.html")
-	})
 	root.Handle(pat.New("/*"), http.FileServer(http.Dir("./assets")))
 }
 
